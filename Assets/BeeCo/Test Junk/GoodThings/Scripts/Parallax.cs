4ad3f6c48521c6553cd8672bad8e9fd15e4247fd
TomRintjema <tomrintjema@hotmail.com>
2016-01-09 11:25:50 -0500

    Deleted RETRO PIXLED
    Put in background

---
+++ Assets/BeeCo/Test Junk/GoodThings/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public Transform[] backgrounds;
+	public float smoothing;
+	private float[] parallaxScales;
+	private Transform camera;
+	private Vector3 previousCameraPosition;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main.transform;
+		previousCameraPosition = camera.position;
+		parallaxScales = new float[backgrounds.Length];
+
+		for (int i = 0; i < backgrounds.Length; i++) {
+			parallaxScales [i] = backgrounds [i].position.z * -1;
+		}	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		for (int i = 0; i < backgrounds.Length; i++) {
+			float parallax = (previousCameraPosition.x - camera.position.x) * parallaxScales [i];
+			float backgroundTargetPositionX = backgrounds [i].position.x + parallax;
+			Vector3 backgroundTargetPosition = new Vector3 (backgroundTargetPositionX, backgrounds [i].position.y, backgrounds [i].position.z);
+
+			backgrounds [i].position = Vector3.Lerp (backgrounds [i].position, backgroundTargetPosition, smoothing * Time.deltaTime);		
+		}
+		previousCameraPosition = camera.position;
+	}
+}
