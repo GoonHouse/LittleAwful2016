3752e49b47c183a8cff029e757aacfbdb6ef6ada
EntranceJew <EntranceJew@gmail.com>
2016-01-08 19:40:12 -0500

    Out: Broken

---
+++ Assets/BeeCo/Scripts/HaggleBall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HaggleBall : MonoBehaviour {
+    public float speed = 30.0f;
+
+    Rigidbody2D rigid;
+    
+	// Use this for initialization
+	void Start () {
+	    rigid = GetComponent<Rigidbody2D>();
+        FuckOff();
+    }
+    
+    // Update is called once per frame
+    void Update() {
+        if( Input.GetKeyDown("space") ){
+            FuckOff();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll){
+        if( coll.gameObject.CompareTag("brick") ){
+            Destroy(coll.gameObject);
+            print("OH BABY IT'S TIME TO SHAVE ME");
+
+            // this.GetComponent<healthScript>().health -= 1;
+        }
+    }
+
+    void FuckOff(){
+        Vector3 v = Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), Vector3.forward) * Vector3.up;
+        rigid.velocity = v * speed;
+    }
+}
---
+++ Assets/BeeCo/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( Input.GetKeyDown("a") ){
+            GetComponent<Rigidbody2D>().velocity = (Vector2.right * speed * -1);
+        } else if(Input.GetKeyDown("d")){
+            GetComponent<Rigidbody2D>().velocity = (Vector2.right * speed);
+        }
+    }
+}
