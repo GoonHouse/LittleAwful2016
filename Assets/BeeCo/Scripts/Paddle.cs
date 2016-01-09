eb3ab399ea4cbb1b4d47115e2a324e873fe34bd5
EntranceJew <EntranceJew@gmail.com>
2016-01-08 20:17:52 -0500

    Amazing hotdog.

--- Assets/BeeCo/Scripts/Paddle.cs
+++ Assets/BeeCo/Scripts/Paddle.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
     public float speed = 10.0f;
+    public float turnAroundBrake = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +12,36 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if( Input.GetKeyDown("a") ){
-            GetComponent<Rigidbody2D>().velocity = (Vector2.right * speed * -1);
-        } else if(Input.GetKeyDown("d")){
-            GetComponent<Rigidbody2D>().velocity = (Vector2.right * speed);
+        // cancel impulse only once
+        if (Input.GetKeyDown("s")){
+            CancelVelocity(true);
+        } else if (Input.GetKeyDown("w")){
+            CancelVelocity(false);
+        }
+
+        // always add velocity
+        if ( Input.GetKey("s") ){
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * -speed);
+        } else if( Input.GetKey("w") ){
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * speed);
+        }
+    }
+
+    bool CancelVelocity(bool down){
+        var rigid = GetComponent<Rigidbody2D>();
+        var vel = rigid.velocity;
+
+        if (
+            (down && vel.y > 0) || 
+            (!down && vel.y < 0)
+            )
+        {
+            Debug.Log("fuck going this way");
+            vel.y = vel.y / turnAroundBrake;
+            rigid.velocity = vel;
+            return true;
+        } else {
+            return false;
         }
     }
 }
