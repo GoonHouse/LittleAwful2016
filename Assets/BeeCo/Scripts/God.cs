9e651d3e19fa0056b14d7902959dc56ffbc9b2f4
EntranceJew <EntranceJew@gmail.com>
2016-01-10 16:33:56 -0500

    Added god object to track things between scenes.

---
+++ Assets/BeeCo/Scripts/God.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+    While most people will discourage the use of god objects, this one exists so that
+    everything doesn't get trashed between scenes and important things can be found always.
+*/
+public class God : MonoBehaviour {
+    public static God main;
+
+    void Awake() {
+        if (main == null) {
+            DontDestroyOnLoad(gameObject);
+            main = this;
+        } else if (main != this) {
+            Debug.Log("False prophet detected.");
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BeeCo/Scripts/LevelTransiton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelTransiton : MonoBehaviour {
+
+    void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Platformer() {
+        SceneManager.LoadScene("platformer");
+    }
+}
