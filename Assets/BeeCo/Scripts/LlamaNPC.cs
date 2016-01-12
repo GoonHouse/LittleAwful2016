91799a0c81ff6001fe61b71aaddebb22aa6aef32
EntranceJew <EntranceJew@gmail.com>
2016-01-12 13:09:31 -0500

    A Talking Llama?!?

--- Assets/BeeCo/Scripts/LlamaNPC.cs
+++ Assets/BeeCo/Scripts/LlamaNPC.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LlamaNPC : MonoBehaviour {
     public GameObject textPrefab;
+    public Text textObj;
 
     public float startPrice = 52.69f;
     public float gameTime = 60.0f;
+
+    public bool isTalking = false;
+    public int textSaidPos = 0;
+    public string textToSay = "Hey buddy you want to buy some :item:?";
+    public float textSpeakSpeed = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +21,39 @@ public class LlamaNPC : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        /*
+	    if( isTalking ) {
+            Talk();
+        } else {
+
+        }
+        */
 	}
 
-    public void Speak(string whatToSay) {
-        var dialog = (GameObject)Instantiate(textPrefab, transform.position, transform.rotation);
-        dialog.GetComponent<FloatTextAway>().SetText(whatToSay, true);
+    public void StartTalking() {
+        isTalking = true;
+        textSaidPos = 0;
+        textPrefab.SetActive(true);
+        textObj.text = "";
+
+        StartCoroutine("Talk");
+        //var dialog = (GameObject)Instantiate(textPrefab, transform.position, transform.rotation);
+        //dialog.GetComponent<FloatTextAway>().SetText(whatToSay, true);
+    }
+
+    public void ShutUp() {
+        isTalking = false;
+        textSaidPos = 0;
+        textPrefab.SetActive(false);
+        textObj.text = "text gone, you shouldn't see this";
+        StopCoroutine("Talk");
+    }
+
+    IEnumerator Talk() {
+        for (int textSaidPos = 0; textSaidPos <= textToSay.Length; textSaidPos++) {
+            textObj.text = textToSay.Substring(0, textSaidPos);
+            GetComponent<AudioSource>().Play();
+            yield return new WaitForSeconds(textSpeakSpeed);
+        }
     }
 }
--- Assets/BeeCo/Scripts/LlamaPlayer.cs
+++ Assets/BeeCo/Scripts/LlamaPlayer.cs
@@ -7,8 +7,9 @@ public class LlamaPlayer : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D coll) {
         var collideParent = coll.gameObject.transform.parent;
         if (collideParent != null && collideParent.gameObject.CompareTag("NPC")) {
+            Debug.Log("touched a " + collideParent.gameObject.name);
             npcToEngage = collideParent.gameObject;
-            npcToEngage.GetComponent<LlamaNPC>().Speak("OY, YOU WANNA CHUMPTY MY FRUMP?");
+            npcToEngage.GetComponent<LlamaNPC>().StartTalking();
         }
     }
 
@@ -19,6 +20,7 @@ public class LlamaPlayer : MonoBehaviour {
             npcToEngage == collideParent.gameObject )
         {
             // @TODO: tell NPC that I'm gone
+            npcToEngage.GetComponent<LlamaNPC>().ShutUp();
             npcToEngage = null;
         }
     }
@@ -30,18 +32,23 @@ public class LlamaPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if( Input.GetKeyDown("f") && npcToEngage != null ) {
+        /*
+        if ( Input.GetKeyDown("f") && npcToEngage != null ) {
             StartMinigame();
         }
+        */
 	}
 
+    public void SaveMyState() {
+        God.playerStats.lastPos = transform.localPosition;
+    }
+
+    /*
     public void StartMinigame() {
         Debug.Log("scream at " + npcToEngage.name);
-        
-        //Let's make sure to get the players current position and information to hold in the gobject
-        God.playerStats.lastPos = transform.localPosition;
 
         var lnpc = npcToEngage.GetComponent<LlamaNPC>();
         God.levelTransition.BreakOut(lnpc.startPrice, lnpc.gameTime);
     }
+    */
 }
