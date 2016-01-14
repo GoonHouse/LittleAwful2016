82a05761f703f331db225402143818d5d88d3c83
EntranceJew <EntranceJew@gmail.com>
2016-01-14 02:18:52 -0500

    Added failure screen.

---
+++ Assets/BeeCo/Scripts/VeryAngryBlock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VeryAngryBlock : MonoBehaviour {
+    public float forceToAdd = 400.0f;
+    Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 v = Quaternion.AngleAxis(90.0f, Vector3.forward) * -Vector3.up;
+        v.x *= forceToAdd;
+        rigid.AddForce(v);
+	}
+}
