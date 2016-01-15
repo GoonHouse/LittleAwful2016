3e847243f0d05b169f3199f375663e90857afe07
EntranceJew <EntranceJew@gmail.com>
2016-01-15 04:38:16 -0500

    Powerup to give more time.

---
+++ Assets/BeeCo/Scripts/Powerups/PowerUpExtraTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpExtraTime : PowerUpItem {
+    public float timeToGive = 30.0f;
+
+    public override void Attach(GameObject attachTo) {
+        paddle = attachTo.GetComponent<Paddle>();
+
+        if (paddle) {
+            God.haggleLogic.time += timeToGive;
+            var t = God.SpawnAt(God.main.hitText, transform.position);
+            var fta = t.GetComponent<FloatTextAway>();
+            fta.SetText("+" + God.FormatTime(timeToGive), true);
+            Destroy(gameObject);
+        } else {
+            base.Attach(attachTo);
+        }
+    }
+}
