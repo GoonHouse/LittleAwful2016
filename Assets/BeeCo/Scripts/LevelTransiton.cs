5701b34891993a9f293907a81099c03504fa592b
phantomfreq <topherpirkl@gmail.com>
2016-01-19 13:19:21 -0600

    added mixer snapshots

--- Assets/BeeCo/Scripts/LevelTransiton.cs
+++ Assets/BeeCo/Scripts/LevelTransiton.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class LevelTransiton : MonoBehaviour {
+    public AudioMixerSnapshot mainGame;
+    public AudioMixerSnapshot ballGame;
 
     // Use this for initialization
     void Start () {
@@ -23,6 +26,7 @@ public class LevelTransiton : MonoBehaviour {
     }
 
     public void Platformer(float adjustMoney = 0.0f, string nextSceneName = "platformer") {
+        mainGame.TransitionTo(1.0f);
         SceneManager.LoadScene(nextSceneName);
         God.playerStats.moneyAdjust = adjustMoney;
     }
@@ -37,6 +41,7 @@ public class LevelTransiton : MonoBehaviour {
             God.playerStats.lastCameraPos = Camera.main.transform.position;
         }
 
+        ballGame.TransitionTo(0.5f);
         SceneManager.LoadScene("breakout");
 
         God.haggleLogic.startPrice = price;
