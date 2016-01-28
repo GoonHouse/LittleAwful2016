d21787f6a9c691ef62eae495d0905210a2028daf
EntranceJew <EntranceJew@gmail.com>
2016-01-28 14:18:09 -0500

    Fixed camera parallax member suppression warning.

--- Assets/BeeCo/Scripts/Camera/Parallax.cs
+++ Assets/BeeCo/Scripts/Camera/Parallax.cs
@@ -7,13 +7,13 @@ public class Parallax : MonoBehaviour {
 	public float smoothing;
 	public float factor = 1f;
 	private float[] parallaxScales;
-	private Transform camera;
+	private Transform myCamera;
 	private Vector3 previousCameraPosition;
 
 	// Use this for initialization
 	void Start () {
-		camera = Camera.main.transform;
-		previousCameraPosition = camera.position;
+		myCamera = Camera.main.transform;
+		previousCameraPosition = myCamera.position;
 		parallaxScales = new float[backgrounds.Length];
 
 		for (int i = 0; i < backgrounds.Length; i++) {
@@ -24,13 +24,13 @@ public class Parallax : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 		for (int i = 0; i < backgrounds.Length; i++) {
-			float parallax = (previousCameraPosition.x - camera.position.x) * parallaxScales [i] * factor;
+			float parallax = (previousCameraPosition.x - myCamera.position.x) * parallaxScales [i] * factor;
 			float backgroundTargetPositionX = backgrounds [i].position.x + parallax;
 			Vector3 backgroundTargetPosition = new Vector3 (backgroundTargetPositionX, backgrounds [i].position.y, backgrounds [i].position.z);
 
 			//backgrounds [i].position = Vector3.Lerp (backgrounds [i].position, backgroundTargetPosition, smoothing * Time.deltaTime);		
 			backgrounds[i].position = backgroundTargetPosition;
 		}
-		previousCameraPosition = camera.position;
+		previousCameraPosition = myCamera.position;
 	}
 }
