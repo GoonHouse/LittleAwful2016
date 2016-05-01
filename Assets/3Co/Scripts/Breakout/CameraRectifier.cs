3e41b7bbc8050408835c2dedbc0124a55c07910b
EntranceJew <EntranceJew@gmail.com>
2016-05-01 15:36:58 -0400

    Added a script to scale the ortho camera for 4:3 windows and 16:9, works for most standard resolutions, I have time for your crazy monitors.

---
+++ Assets/3Co/Scripts/Breakout/CameraRectifier.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRectifier : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        // why do these numbers work? I don't fuckin know.
+        Camera.main.orthographicSize = God.Scale(Camera.main.aspect, 5.0f / 4.0f, 16.0f / 9.0f, 7.2f, 5.0f);
+	}
+}
