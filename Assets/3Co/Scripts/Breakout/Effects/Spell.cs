aecbca6a3960a02b97b4010b1c80412dc45ddca0
EntranceJew <EntranceJew@gmail.com>
2016-05-11 13:36:27 -0400

    Made the roach cast a spell that only embiggens the ball when it arrives.

---
+++ Assets/3Co/Scripts/Breakout/Effects/Spell.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IOnCast {
+    void Action(Transform origin, Transform destination);
+}
+
+public interface IOnArrive {
+    void Action(Transform origin, Transform destination);
+}
+
+public class TestArriveEffect : IOnArrive {
+    public void Action(Transform origin, Transform destination) {
+        var go = (GameObject)GameObject.Instantiate(
+            Resources.Load("Effects/BurstEffect") as GameObject,
+            destination.position,
+            destination.rotation
+        );
+        go.transform.SetParent(destination);
+    }
+}
+
+public class Spell : MonoBehaviour {
+    public List<IOnCast> OnCastEffects = new List<IOnCast>();
+    public List<IOnArrive> OnArriveEffects = new List<IOnArrive>();
+    public Transform origin;
+    public Transform destination;
+    public float travelTime;
+
+    private float timeTraveled;
+    private bool wasCast = false;
+
+    public void Cast(Transform origin, Transform destination, float travelTime) {
+        this.origin = origin;
+        this.destination = destination;
+        this.travelTime = travelTime;
+
+        wasCast = true;
+
+        foreach (IOnCast oc in OnCastEffects) {
+            oc.Action(origin, destination);
+        }
+    }
+
+    public void Update() {
+        if( wasCast ) {
+            timeTraveled += Time.deltaTime;
+
+            transform.position = Vector3.Lerp(origin.position, destination.position, timeTraveled / travelTime);
+
+            if( timeTraveled >= travelTime) {
+                foreach (IOnArrive oa in OnArriveEffects) {
+                    oa.Action(origin, destination);
+                }
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/3Co/Scripts/Breakout/Familiar/AFamiliar.cs
+++ Assets/3Co/Scripts/Breakout/Familiar/AFamiliar.cs
@@ -1,6 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class BallEmbiggener : IOnArrive {
+    public void Action(Transform origin, Transform destination) {
+        var scale = destination.localScale;
+        scale *= 1.25f;
+        destination.localScale = scale;
+    }
+}
+
 public class AFamiliar : BaseFamiliar {
     // don't overwrite nothing because we do jack and also shit
+    //public class Base
+    override public bool DoAbility(AbstractPlayer player, Vector3 pos) {
+        var ce = (GameObject)Instantiate(
+            Resources.Load("Effects/CastEffect") as GameObject,
+            player.transform.position,
+            player.transform.rotation
+        );
+        var spell = ce.GetComponent<Spell>();
+
+        spell.OnArriveEffects.Add(new TestArriveEffect());
+        spell.OnArriveEffects.Add(new BallEmbiggener());
+
+        spell.Cast(player.transform, player.focusedThing.transform, 1.0f);
+        
+        //var ball = player.focusedThing;
+        //var scale = ball.transform.localScale;
+        //scale *= 1.25f;
+        //ball.transform.localScale = scale;
+        return true;
+    }
 }
