c8ff25f7f2fbbbb50760eb90ae3fd0967116f77c
EntranceJew <EntranceJew@gmail.com>
2016-05-11 05:57:13 -0400

    Made it so that ball tail girth is proportional to its scale. GirthToggler now merely maintains the girth.

---
+++ Assets/3Co/Scripts/Breakout/Ball/GirthMaintainer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GirthMaintainer : MonoBehaviour {
+    public float maxScaleRatio = 2.0f;
+
+    private TrailRenderer tr;
+
+    // Use this for initialization
+    void Start() {
+        tr = GetComponent<TrailRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        tr.startWidth = transform.parent.localScale.x * maxScaleRatio;
+    }
+}
--- Assets/3Co/Scripts/Breakout/Familiar/BallsFamiliar.cs
+++ Assets/3Co/Scripts/Breakout/Familiar/BallsFamiliar.cs
@@ -4,13 +4,11 @@ using System.Collections;
 public class BallsFamiliar : BaseFamiliar {
 
     override public bool DoAbility(AbstractPlayer player, Vector3 pos) {
-        var npos = transform.position;
-        npos.x += 1.0f;
         if( !player.CanSpawnSomethingHere(pos) ) {
             return false;
         }
-        var tball = (GameObject)Instantiate(player.ball, npos, Quaternion.identity);
-        tball.transform.SetParent(GameObject.Find("BallHell").transform, true);
+        var tball = (GameObject)Instantiate(player.ball, transform.position, Quaternion.identity);
+        tball.transform.SetParent(GameObject.Find("BallHell").transform, false);
         tball.GetComponent<PowerupManager>().owner = gameObject;
         tball.GetComponent<BaseBall>().owner = gameObject;
         tball.GetComponent<BaseBall>().creator = player;
